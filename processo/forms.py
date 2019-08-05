3ff821ed5d89695d8867188c868b87d02c462040
martinsmessias <messiasads2017@gmail.com>
2019-08-05 16:35:55 -0500

    Versão 3

--- processo/forms.py
+++ processo/forms.py
@@ -11,3 +11,9 @@ class ProcessoForm(forms.ModelForm):
                   'nome_parte_processo', 'assunto_processo',
                   'data_abertura_processo', 'numero_caixa_processo',
                   'divisao_processo', 'tipo_processo', 'arquivo_processo')
+
+        def _init_(self, *args, **kwargs):
+            for l in self.base_fields:
+                self.base_fields[l].widget.attrs['class'] = '"form-control"'
+
+            super(ProcessoForm, self)._init_(*args, **kwargs)
