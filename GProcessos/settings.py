e653557dda1a9d56046222f6c78945b2145e3086
martinsmessias <messiasads2017@gmail.com>
2019-09-25 10:54:27 -0500

    1.9.25 - Heroku Deploy Test

--- GProcessos/settings.py
+++ GProcessos/settings.py
@@ -1,12 +1,8 @@
 import os
 from django.core.management.utils import get_random_secret_key
 
-try:
-    import django_heroku
-    import dj_database_url
-except ModuleNotFoundError:
-    pass
-
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
