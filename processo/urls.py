bf3f77943fd33e2540c9163dca1cbb89f57c27f1
martinsmessias <messiasads2017@gmail.com>
2019-09-25 11:14:59 -0500

    1.9.25 - Heroku Deploy Test

--- processo/urls.py
+++ processo/urls.py
@@ -5,19 +5,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-                  path('login/', views.login, name='login'),
-                  path('', views.index, name='index'),
-                  path('divisao/', views.divisao, name='divisao'),
-                  path('editar_divisao/<str:id>', views.editar_divisao, name='editar_divisao'),
-                  path('excluir_div/<str:id>', views.excluir_div, name='excluir_divisao'),
-                  path('cadastrar_div/', views.cadastrar_div, name='cadastrar_div'),
-                  path('tipo/', views.tipo, name='tipo'),
-                  path('editar_tipo/<str:id>', views.editar_tipo, name='editar_tipo'),
-                  path('excluir_tipo/<str:id>', views.excluir_tipo, name='excluir_tipo'),
-                  path('cadastrar_tipo/', views.cadastrar_tipo, name='cadastrar_tipo'),
-                  path('cadastrar/', views.cadastrar, name='cadastrar'),
-                  path('editar/<str:id>', views.editar, name='editar'),
-                  path('caixas/', views.caixas, name='caixas'),
-                  path('excluir/<str:id>', views.excluir, name='excluir'),
-                  path('accounts/', views.accounts, name='accounts')
+                  path('/login/', views.login, name='login'),
+                  path('/index/', views.index, name='index'),
+                  path('/divisao/', views.divisao, name='divisao'),
+                  path('/editar_divisao/<str:id>', views.editar_divisao, name='editar_divisao'),
+                  path('/excluir_div/<str:id>', views.excluir_div, name='excluir_divisao'),
+                  path('/cadastrar_div/', views.cadastrar_div, name='cadastrar_div'),
+                  path('/tipo/', views.tipo, name='tipo'),
+                  path('/editar_tipo/<str:id>', views.editar_tipo, name='editar_tipo'),
+                  path('/excluir_tipo/<str:id>', views.excluir_tipo, name='excluir_tipo'),
+                  path('/cadastrar_tipo/', views.cadastrar_tipo, name='cadastrar_tipo'),
+                  path('/cadastrar/', views.cadastrar, name='cadastrar'),
+                  path('/editar/<str:id>', views.editar, name='editar'),
+                  path('/caixas/', views.caixas, name='caixas'),
+                  path('/excluir/<str:id>', views.excluir, name='excluir'),
+                  path('/accounts/', views.accounts, name='accounts')
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
