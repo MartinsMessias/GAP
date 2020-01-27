31789579b74e058972caa87008307fd95499054d
martinsmessias <messiasads2017@gmail.com>
2020-01-27 14:18:56 -0500

    Edições em SEC_KEY

--- key.py
+++ key.py
@@ -2,8 +2,6 @@ import os
 from django.core.management.utils import get_random_secret_key
 
 
-try:
-    print(os.environ['KEY'])
-except KeyError:
-    KEY = get_random_secret_key()
-    os.environ['KEY'] = KEY
+KEY = get_random_secret_key()
+os.environ['KEY'] = KEY
+print(os.environ['KEY'])
