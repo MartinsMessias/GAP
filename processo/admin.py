e972250002ab41e6e31f5971384239bc09caad79
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2020-05-28 19:33:56 -0500

    Update admin.py

--- processo/admin.py
+++ processo/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Divisao)
-admin.site.register(Processo)
-admin.site.register(TipoDeProcesso)
+# admin.site.register(Divisao)
+# admin.site.register(Processo)
+# admin.site.register(TipoDeProcesso)
